1196aa6802a3f415090bbfcbbfe76f2e7bbffe95
Delbo <delbosilvaster@gmail.com>
2020-06-23 22:13:38 -0300

    Projeto elaborado para ler dados de um pedido fornecido pelo usuário e fornecer sua lista do pedido

---
+++ ProjetoE/Entities/Client.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjetoE.Entities
+{
+    class Client
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate  { get; set; }
+
+        public  Client ()
+        {
+        }
+
+        public Client(string name, string email, DateTime birthdate)
+        {
+            Name = name;
+            Email = email;
+            BirthDate = birthdate;
+        }
+
+        public override String ToString()
+        {
+            return Name
+                + ", ("
+                + BirthDate.ToString("dd/mm/wwww")
+                + ") - "
+                + Email;
+        }
+    }
+}
